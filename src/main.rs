6d966c5fc23e4a951687f28730177b15c1325936
lieven <lieven.merckx@gmail.com>
2023-05-05 17:54:40 +0200

    container central, still no grid

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,73 @@ use std::boxed::Box;
 use std::thread::sleep;
 use std::time::Duration;
 use std::time::Instant;
+use yaml_rust::YamlLoader;
 
 fn main() -> LvResult<()> {
+    set_logging();
+    load_yaml();
+    info!("Starting up");
+    const HOR_RES: u32 = 1024;
+    const VER_RES: u32 = 768;
+
+    let buffer = DrawBuffer::<{ (HOR_RES * VER_RES / 10) as usize }>::default();
+    let display = lv_drv_disp_gtk!(buffer, HOR_RES, VER_RES)?; // Use this for GTK (Linux)
+    let _input = lv_drv_input_pointer_gtk!(display)?;
+
+    // Create screen and widgets
+    let mut screen = display.get_scr_act()?;
+    // cont.set_size(300, 200)?;
+
+    let mut cont = Obj::create(&mut screen)?;
+
+    let mut cont_style = Style::default();
+
+    unsafe {
+        let x_grid = CoordDesc::<3>::from_values([70, 70, 70], true);
+        let y_grid = CoordDesc::<3>::from_values([50, 50, 50], true);
+        cont_style.set_grid_column_dsc_array(&x_grid);
+        cont_style.set_grid_row_dsc_array(&y_grid);
+    }
+    cont_style.set_align(Align::Center);
+    cont_style.set_layout(Layout::grid());
+    cont_style.set_width(300);
+    cont_style.set_height(200);
+
+    cont.add_style(Part::Main, &mut cont_style)?;
+
+    let mut buttons = Vec::new();
+
+    for i in 0..9 {
+        let col = i % 3;
+        let row = i / 3;
+
+        let mut btn = Btn::create(&mut cont)?;
+        let mut btn_style = Style::default();
+        btn_style.set_grid_cell_column_pos(col);
+        btn_style.set_grid_cell_row_pos(row);
+        btn_style.set_grid_cell_column_span(2);
+        btn_style.set_grid_cell_row_span(2);
+        btn_style.set_width(70);
+        btn_style.set_height(50);
+        info!(" row {} col {}", row, col);
+        btn.add_style(Part::Any, &mut btn_style)?;
+
+        let mut label = Label::create(&mut btn)?;
+        let s = CString::new(format!("c{}, r{} ", col, row)).unwrap();
+        label.set_text(&s)?;
+        label.set_align(Align::Center, 0, 0)?;
+        buttons.push(btn);
+    }
+    info!("start loop");
+    loop {
+        let start = Instant::now();
+        lvgl::task_handler();
+        sleep(Duration::from_millis(15));
+        lvgl::tick_inc(Instant::now().duration_since(start));
+    }
+}
+
+fn set_logging() {
     let console = ConsoleAppender::builder()
         .encoder(Box::new(PatternEncoder::new(
             "{d(%Y-%m-%d %H:%M:%S.%3f)} {l} {t} - {m}\n",
@@ -37,57 +102,19 @@ fn main() -> LvResult<()> {
         .unwrap();
 
     log4rs::init_config(config).unwrap();
-    info!("Starting up");
-    const HOR_RES: u32 = 1024;
-    const VER_RES: u32 = 768;
+}
 
-    let buffer = DrawBuffer::<{ (HOR_RES * VER_RES /10 ) as usize }>::default();
-    let display = lv_drv_disp_gtk!(buffer, HOR_RES, VER_RES)?; // Use this for GTK (Linux)
-    let _input = lv_drv_input_pointer_gtk!(display)?;
+fn load_yaml() -> Vec<yaml_rust::Yaml> {
+    let source = "
+person:
+    - name: John Doe
+      age: 43
+      phones:
+        - \"555-555-5555\"
+        - \"666-666-6666\"
+";
+    let docs = YamlLoader::load_from_str(source).unwrap();
+    info!("Yaml docs: {:?}", docs);
 
-    // Create screen and widgets
-    let mut screen = display.get_scr_act()?;
-
-    // let cont = Widget::create(&mut screen)?;
-
-    let mut screen_style = Style::default();
-
-    unsafe {
-        let x_grid = CoordDesc::<3>::from_values([70, 70, 70], true);
-        let y_grid = CoordDesc::<3>::from_values([50, 50, 50], true);
-        screen_style.set_grid_column_dsc_array(&x_grid);
-        screen_style.set_grid_row_dsc_array(&y_grid);
-    }
-    screen.set_size(300,220)?;
-    screen_style.set_layout(Layout::grid());
-
-    screen.add_style(Part::Main, &mut screen_style)?;
-
-    for i in 0..9 {
-        let col = i % 3;
-        let row = i / 3;
-
-        let mut btn = Btn::create(&mut screen)?;
-        let mut btn_style = Style::default();
-        btn_style.set_grid_cell_column_pos(col);
-        btn_style.set_grid_cell_row_pos(row);
-        btn_style.set_grid_cell_column_span(1);
-        btn_style.set_grid_cell_row_span(1);
-        btn_style.set_width(70);
-        btn_style.set_height(50);
-
-        btn.add_style(Part::Any, &mut btn_style)?;
-
-        let mut label = Label::create(&mut btn)?;
-        let s = CString::new(format!("c{}, r{} ", col, row)).unwrap();
-        label.set_text(&s)?;
-        label.set_align(Align::Center, 0, 0)?;
-    }
-
-    loop {
-        let start = Instant::now();
-        lvgl::task_handler();
-        sleep(Duration::from_millis(15));
-        lvgl::tick_inc(Instant::now().duration_since(start));
-    }
+    docs
 }
