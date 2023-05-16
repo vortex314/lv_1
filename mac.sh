4058e48583ce77287854b5f9b90a0ada639033b4
Lieven Merckx <lieven.merckx@ing.com>
2023-05-16 19:02:22 +0200

    macos fails

--- mac.sh
+++ mac.sh
@@ -1,9 +1,8 @@
 set -x
-TOOLCHAIN=arm-linux-gnueabihf
-TOOLCHAIN_DIR=$HOME/toolchain/$TOOLCHAIN        # where the toolchain is installed (see below)
+# TOOLCHAIN=arm-linux-gnueabihf
 export DEP_LV_CONFIG_PATH=`pwd`/lv_sdl           # location of the lvgl config file lv_conf.h lv_drv_conf.h
-export LVGL_LINK="SDL,SDL2"
-export LIBCLANG_PATH="/Library/Developer/CommandLineTools/usr/lib/"
-# cargo clean
-cargo build --release  -j 4
+# export LVGL_LINK="SDL,SDL2"
+# export LIBCLANG_PATH="/Library/Developer/CommandLineTools/usr/lib/"
+cargo clean
+cargo build  -j 4
 
